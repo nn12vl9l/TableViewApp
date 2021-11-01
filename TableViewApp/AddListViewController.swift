40e5f20b41e9f1ee4e567761eb6aae197602cb7a
nn12vl9l <nn12vl9lsoft@gmail.com>
2021-11-01 18:57:25 +0900

    未入力で投稿できない処理

--- TableViewApp/AddListViewController.swift
+++ TableViewApp/AddListViewController.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 class AddListViewController: UIViewController {
-
+    
     @IBOutlet weak var addTextfield: UITextField!
     var taskArray: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         let userDefaults = UserDefaults.standard
         
         if userDefaults.object(forKey: "add") != nil {
@@ -23,23 +23,25 @@ class AddListViewController: UIViewController {
     }
     
     @IBAction func addTask(_ sender: Any) {
-
+        
         let userDefaults = UserDefaults.standard
-        taskArray.append(addTextfield.text!)
+        if(addTextfield.text! != ""){
+            taskArray.append(addTextfield.text!)
+        }
         
         userDefaults.set(taskArray, forKey: "add")
         self.navigationController?.popViewController(animated: true)
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
